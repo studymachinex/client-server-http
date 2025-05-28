9301e84cd2b2a5e34d0a55121e9e9fac9e85c855
studymachinex <carlosufpa10@gmail.com>
2025-05-28 16:28:19 -0300

    implementa requisicao no client e escrita da resposta da api em arquivo txt

--- client/main.go
+++ client/main.go
@@ -5,31 +5,50 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
-	defer cancel()
+	resp, err := getCotacaoDolar()
+	if err != nil {
+		log.Fatal(err)
+	}
+	salvarCotacaoEmArquivo(resp)
+	log.Printf("Cotação do dólar: R$ %s", resp)
 
-	getCotacaoDolar(ctx)
-	log.Println("Requisição concluída")
 }
 
-func getCotacaoDolar(ctx context.Context) (string, error) {
-	apiUrl := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, apiUrl, nil)
+func salvarCotacaoEmArquivo(resp string) {
+	file, err := os.Create("cotacao.txt")
 	if err != nil {
-		log.Fatalf("Erro ao fazer requisição: %v", err)
+		log.Fatalf("Erro ao criar arquivo: %v", err)
 	}
+	defer file.Close()
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	_, err = file.WriteString(resp)
 	if err != nil {
-		return "", err
+		log.Fatalf("Erro ao escrever no arquivo: %v", err)
+	}
+	log.Println("Cotação salva em cotacao.txt")
+}
+
+func getCotacaoDolar() (string, error) {
+	apiUrl := "http://localhost:8080/cotacao"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
+	defer cancel()
+	
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, apiUrl, nil)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Timeout: tempo de execução insuficiente para buscar dados da API")
+		}
+		return "erro ao realizar a requisicao: ", err
 	}
 	defer resp.Body.Close()
 
@@ -37,10 +56,14 @@ func getCotacaoDolar(ctx context.Context) (string, error) {
 		return "", fmt.Errorf("erro ao obter cotação: %s", resp.Status)
 	}
 
-	var response shared.DolarApiResponse
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return "", err
+	byteData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "erro ao ler resposta: ", err
 	}
-
-	return response.USDBRL.Bid, nil
+	var c shared.SimpleBidResponse
+	err = json.Unmarshal(byteData, &c)
+	if err != nil {
+		return "erro ao decodificar resposta: ", err
+	}
+	return fmt.Sprintf("Dólar: %s", c.Bid), nil
 }
--- shared/model.go
+++ shared/model.go
@@ -5,3 +5,10 @@ type DolarApiResponse struct {
 		Bid string `json:"bid"`
 	} `json:"USDBRL"`
 }
+
+// Nova struct para resposta simples da API
+// Apenas o campo bid
+
+type SimpleBidResponse struct {
+	Bid string `json:"bid"`
+}
